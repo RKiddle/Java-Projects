8c5ac6f971f72a674357d6c635d1c7d3281a609a
Dr Richard Kiddle <RKiddle@users.noreply.github.com>
2025-02-20 11:18:30 +0700

    First and second weeks

---
+++ Intro-to-Java-IBM/DataTypes/CharToToInt.java
@@ -0,0 +1,7 @@
+public class CharToToInt {
+    public static void main(String s[]) {
+        char my_char = 'a';
+        System.out.println("Value of my_char as character  " + my_char);
+        System.out.println("Value of my_char cast as intger  " + (int)my_char);
+    }
+}
---
+++ Intro-to-Java-IBM/Operators/ArrayOperator.java
@@ -0,0 +1,25 @@
+
+public class ArrayOperator {
+    public static void main(String[] args) {
+
+        // Make Array
+        int[] myArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        // Addition
+        System.out.println("Addition: + operator " + (myArray[1] + myArray[2]) );
+        // Subtraction
+        System.out.println("Subtraction: - operator " + (num1 - num2) );
+        // Multiplication
+        System.out.println("Multiplication: * operator " + (num1 * num2) );
+        // Division
+        System.out.println("Division: / operator " + (num1 / num2) );
+        // Modulus (remainder)
+        System.out.println("Modulus: % operator " + (num1 % num2) );
+
+        /* Use a for loop to check if the next number in the array is greater than, less than or equal to the current number. 
+        User ternary and relational operator.
+        */
+
+    }
+
+}
