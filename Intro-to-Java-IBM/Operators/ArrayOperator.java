ef8b55554a08f2bc792dd0a5a499f9ff46a63440
Dr Richard Kiddle <RKiddle@users.noreply.github.com>
2025-02-21 10:05:38 +0700

    Update ArrayOperator.java

--- Intro-to-Java-IBM/Operators/ArrayOperator.java
+++ Intro-to-Java-IBM/Operators/ArrayOperator.java
@@ -6,19 +6,24 @@ public class ArrayOperator {
         int[] myArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
         // Addition
-        System.out.println("Addition: + operator " + (myArray[1] + myArray[2]) );
+        System.out.println("Addition: + operator " + (myArray[0] + myArray[1]) );
         // Subtraction
-        System.out.println("Subtraction: - operator " + (num1 - num2) );
+        System.out.println("Subtraction: - operator " + (myArray[2] - myArray[3]) );
         // Multiplication
-        System.out.println("Multiplication: * operator " + (num1 * num2) );
+        System.out.println("Multiplication: * operator " + (myArray[4] * myArray[5]) );
         // Division
-        System.out.println("Division: / operator " + (num1 / num2) );
+        System.out.println("Division: / operator " + (myArray[6] / myArray[7]) );
         // Modulus (remainder)
-        System.out.println("Modulus: % operator " + (num1 % num2) );
+        System.out.println("Modulus: % operator " + (myArray[8] % myArray[9]) );
 
         /* Use a for loop to check if the next number in the array is greater than, less than or equal to the current number. 
         User ternary and relational operator.
         */
+        for (int i = 0; i < myArray.length - 1; i++) {
+            String comparison = (myArray[i] < myArray[i + 1]) ? "less than" :
+                                (myArray[i] > myArray[i + 1]) ? "greater than" : "equal to";
+            System.out.println("Element " + myArray[i] + " is " + comparison + " element " + myArray[i + 1]);
+        }
 
     }
 
